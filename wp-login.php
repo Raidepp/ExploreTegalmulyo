5c365cea51d6ed1cb2729d501ab327ade3dcf568
RaidRafif <raid.rafif3@gmail.com>
2023-01-05 23:15:40 +0700

    First stage of website. Polish next

--- wp-login.php
+++ wp-login.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
-<html lang="en-US">
+<html dir="ltr" lang="en-US" prefix="og: https://ogp.me/ns#">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Log In &lsaquo; Explore Tegalmulyo &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
-<link rel="stylesheet" id="dashicons-css" href="./wp-includes/css/dashicons.min.css?ver=6.1.1" media="all">
-<link rel="stylesheet" id="buttons-css" href="./wp-includes/css/buttons.min.css?ver=6.1.1" media="all">
-<link rel="stylesheet" id="forms-css" href="./wp-admin/css/forms.min.css?ver=6.1.1" media="all">
-<link rel="stylesheet" id="l10n-css" href="./wp-admin/css/l10n.min.css?ver=6.1.1" media="all">
-<link rel="stylesheet" id="login-css" href="./wp-admin/css/login.min.css?ver=6.1.1" media="all">
+<link rel="stylesheet" id="dashicons-css" href="/wp-includes/css/dashicons.min.css?ver=6.1.1" media="all">
+<link rel="stylesheet" id="buttons-css" href="/wp-includes/css/buttons.min.css?ver=6.1.1" media="all">
+<link rel="stylesheet" id="forms-css" href="/wp-admin/css/forms.min.css?ver=6.1.1" media="all">
+<link rel="stylesheet" id="l10n-css" href="/wp-admin/css/l10n.min.css?ver=6.1.1" media="all">
+<link rel="stylesheet" id="login-css" href="/wp-admin/css/login.min.css?ver=6.1.1" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
-	<link rel="icon" href="./wp-content/uploads/2023/01/cropped-fix-no-bg-32x32.png" sizes="32x32">
-<link rel="icon" href="./wp-content/uploads/2023/01/cropped-fix-no-bg-192x192.png" sizes="192x192">
-<link rel="apple-touch-icon" href="./wp-content/uploads/2023/01/cropped-fix-no-bg-180x180.png">
-<meta name="msapplication-TileImage" content="./wp-content/uploads/2023/01/cropped-fix-no-bg-270x270.png">
-	</head>
+		</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
 	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
 		<div id="login">
@@ -45,31 +41,31 @@
 		</form>
 
 					<p id="nav">
-				<a href="./?action=lostpassword">Lost your password?</a>			</p>
+				<a href="/wp-login.php?action=lostpassword">Lost your password?</a>			</p>
 					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 				<p id="backtoblog">
-			<a href="./index.html">&larr; Go to Explore Tegalmulyo</a>		</p>
+			<a href="/">&larr; Go to Explore Tegalmulyo</a>		</p>
 			</div>
-			<script src="./wp-includes/js/jquery/jquery.min.js?ver=3.6.1" id="jquery-core-js"></script>
-<script src="./wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
-<script id="zxcvbn-async-js-extra">var _zxcvbnSettings = {"src":".\/wp-includes\/js\/zxcvbn.min.js"};</script>
-<script src="./wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="./wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
-<script src="./wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script src="./wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
-<script src="./wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
+			<script src="/wp-includes/js/jquery/jquery.min.js?ver=3.6.1" id="jquery-core-js"></script>
+<script src="/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">var _zxcvbnSettings = {"src":"httplocalhost\/exploretegalmulyo\/wp-includes\/js\/zxcvbn.min.js"};</script>
+<script src="/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script src="/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
+<script src="/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
 <script id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
 <script id="password-strength-meter-js-extra">var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};</script>
-<script src="./wp-admin/js/password-strength-meter.min.js?ver=6.1.1" id="password-strength-meter-js"></script>
-<script src="./wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script src="/wp-admin/js/password-strength-meter.min.js?ver=6.1.1" id="password-strength-meter-js"></script>
+<script src="/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
 <script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/exploretegalmulyo\/wp-admin\/admin-ajax.php"}};</script>
-<script src="./wp-includes/js/wp-util.min.js?ver=6.1.1" id="wp-util-js"></script>
-<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"bb46b49f9c"};</script>
-<script src="./wp-admin/js/user-profile.min.js?ver=6.1.1" id="user-profile-js"></script>
+<script src="/wp-includes/js/wp-util.min.js?ver=6.1.1" id="wp-util-js"></script>
+<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"7dd3e1e0d1"};</script>
+<script src="/wp-admin/js/user-profile.min.js?ver=6.1.1" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
